0e864145f8fd76fd6e7a6c2d110edf90d10708b9
sachinm78 <sachinm78@gmail.com>
2020-05-19 13:47:39 -0500

    created App title and set up basic styling

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./styles.css"
+
+function App() {
+    return (
+        <div className="container">
+            <h1 className="title">React Movie Search</h1>
+        </div>
+    )
+}
+
+export default App
