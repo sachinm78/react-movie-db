3976a85e270855782e6fbb62c524841c5ecc6eba
sachinm78 <sachinm78@gmail.com>
2020-05-19 13:30:54 -0500

    updated packages and set up app to begin building it out

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"))
+class Main extends React.Component {
+  render() {
+    return (
+      <h1>Hello world updated!</h1>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
